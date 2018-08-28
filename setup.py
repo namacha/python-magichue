cc31190022a17bb7a6868a2127fef768970ede95
ayu <mac.ayu15@gmail.com>
2018-08-29 00:27:06 +0900

    added pip files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+import magichue
+
+
+setup(
+    name='python-magichue',
+    version='0.2.1',
+    url='https://github.com/namacha/python-magichue',
+    author=magichue.__author__,
+    author_email='mac.ayu15@gmail.com',
+    long_description_content_type='text/markdown',
+    description='A library to interface with Magichue(or Magichome)',
+    long_description=open('README.md').read(),
+    license=magichue.__license__,
+    
+    packages=find_packages(),
+)
