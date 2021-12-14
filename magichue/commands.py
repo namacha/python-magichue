f325c56ebfa83a419cc18d8123445f9a99d8ce48
namacha <mac.ayu15@gmail.com>
2021-12-14 13:50:14 +0900

    add: remote client

--- magichue/commands.py
+++ magichue/commands.py
@@ -1,3 +1,62 @@
+import struct
+from typing import List
+
+
+class Command:
+
+    array: List[int]
+    response_length: int
+
+    @staticmethod
+    def append_terminator(arr, is_remote):
+        return arr + [0xf0 if is_remote else 0x0f]
+
+    @staticmethod
+    def calc_checksum(arr):
+        hex_checksum = hex(sum(arr))[-2:]
+        checksum = int(hex_checksum, 16)
+        return checksum
+
+    @classmethod
+    def attach_checksum(cls, arr):
+        return arr + [cls.calc_checksum(arr)]
+
+    @classmethod
+    def hex_array(cls, is_remote: bool=False):
+        return cls.attach_checksum(
+            append_terminator(cls.array, is_remote)
+        )
+
+    @classmethod
+    def byte_string(cls, is_remote: bool=False):
+        _arr = cls.attach_checksum(
+            cls.append_terminator(cls.array, is_remote)
+        )
+        return struct.pack('!%dB' % (len(_arr)+1), *_arr)
+
+    @classmethod
+    def hex_string(cls, is_remote: bool=False):
+        _arr = cls.attach_checksum(
+            cls.append_terminator(cls.array, is_remote)
+        )
+        return ''.join([hex(v)[2:].zfill(2) for v in _arr])
+
+
+class TurnON(Command):
+    array = [0x71, 0x23]
+    response_length = 14
+
+
+class TurnOFF(Command):
+    array = [0x71, 0x24]
+    response_length = 14
+
+
+class QueryStatus(Command):
+    array = [0x81, 0x8a, 0x8b]
+    response_length = 14
+
+
 QUERY_STATUS_1 = 0x81
 QUERY_STATUS_2 = 0x8a
 QUERY_STATUS_3 = 0x8b
---
+++ magichue/http_api.py
@@ -0,0 +1,111 @@
+import random
+import logging
+import hashlib
+from string import (
+    ascii_uppercase,
+    digits
+)
+import requests
+
+
+API_BASE = 'https://wifij01us.magichue.net/app'
+UA = 'Magic Hue/1.2.2 (IOS,13.400000,ja_JP)'
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class HTTPError(Exception):
+    pass
+
+
+class MagicHueAPIError(Exception):
+    pass
+
+
+class RemoteAPI:
+
+    def __init__(self, token):
+        self.token = token
+
+    @classmethod
+    def auth(
+        cls,
+        user: str,
+        password: str,
+        client_id : str = ''
+    ):
+        if not client_id:
+            client_id = ''.join(
+                [random.choice(ascii_uppercase + digits) for _ in range(32)]
+            )
+        payload = {
+            'userID': user,
+            'password': hashlib.md5(password.encode('utf8')).hexdigest(),
+            'clientID': client_id,
+        }
+        _LOGGER.debug(f'Logging in with email {user}')
+        res = requests.post(
+            API_BASE+'/login/MagicHue',
+            json=payload,
+            headers={'User-Agent': UA}
+        )
+        if res.status_code != 200:
+            raise HTTPError
+        if res.json().get('code') != 0:
+            raise MagicHueAPIError(res.json().get('msg'))
+        _LOGGER.debug(f'Login successful')
+        return res.json().get('token')
+
+    @classmethod
+    def login_with_user_password(
+        cls,
+        user: str,
+        password: str,
+        client_id : str = ''
+    ):
+        token = cls.auth(user, password, client_id)
+        return RemoteAPI(token=token)
+
+    @classmethod
+    def login_with_token(
+        cls,
+        token: str
+    ):
+        return RemoteAPI(token)
+
+    def _post_with_token(self, endpoint, payload):
+        _LOGGER.debug(f'Sending POST request to {endpoint}, payload={payload}')
+        res = requests.post(
+            API_BASE + endpoint,
+            json=payload,
+            headers={'User-Agent': UA, 'token':self.token}
+        )
+        if res.status_code != 200:
+            raise HTTPError
+        if res.json().get('code') != 0:
+            raise MagicHueAPIError(res.json().get('msg'))
+        return res.json()
+
+    def _get_with_token(self, endpoint):
+        _LOGGER.debug(f'Sending GET request to {endpoint}')
+        res = requests.get(
+            API_BASE + endpoint,
+            headers={'User-Agent': UA, 'token':self.token}
+        )
+        if res.status_code != 200:
+            raise HTTPError
+        if res.json().get('code') != 0:
+            raise MagicHueAPIError(res.json().get('msg'))
+        return res.json()
+
+    def _send_request(self, data):
+        result = self._post_with_token('/sendRequestCommand/MagicHue', data)
+        return result
+
+    def _send_command(self, data):
+        result = self._post_with_token('/sendCommandBatch/MagicHue', data)
+        return result
+
+    def get_devices(self):
+        result = self._get_with_token('/getMyBindDevicesAndState/MagicHue')
+        return result
